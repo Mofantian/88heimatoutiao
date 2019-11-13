342c7dbd6293a0889ad0aa46b5cfefccde0fa9f6
Mofantian <2711318375@qq.com>
2019-11-13 14:54:20 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    主页
-  </div>
+  <el-container>
+    <!-- 左侧容器 -->
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <!-- 头部容器 -->
+      <el-header>Header</el-header>
+      <el-main>
+        <!-- 中间内容容器 -->
+        这里面放二级路由
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>

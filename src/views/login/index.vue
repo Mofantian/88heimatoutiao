0bc1c0c8ab6f9effe87e24ad50bdbf2ef1b56054
Mofantian <2711318375@qq.com>
2019-11-13 10:23:32 +0800

    ‘登录模块-页面布局及样式’

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-      登录
+  <div class="login">
+    <!-- element ui 卡片组件 -->
+    <el-card class="login-card">
+      <!-- 卡片内容 -->
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt="">
+      </div>
+      <el-form style="margin-top:30px">
+        <el-form-item>
+          <!-- 放置手机号 -->
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <!-- 放置验证码 -->
+          <el-input style="width:280px" placeholder="请输入验证码"></el-input>
+          <el-button style="float:right" plain>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议及条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -10,6 +33,23 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+ .login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; // 占当前屏幕的百分百高度
+    background: url('../../assets/img/login_bg.jpg');
+    background-size: cover;
+    .login-card {
+      width: 440px;
+      height: 340px;
+      .title {
+        text-align: center;
+        img {
+          height: 45px;
+        }
+      }
+    }
+ }
 </style>

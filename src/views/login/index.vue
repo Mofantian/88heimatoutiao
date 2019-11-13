24cb49c6fa959e296299b714252883f1bc6af54d
Mofantian <2711318375@qq.com>
2019-11-13 11:53:26 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,21 +6,41 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
-      <el-form style="margin-top:30px">
-        <el-form-item>
+      <!-- model属性 首先要绑定表单数据对象 -->
+      <!-- rules属性 校验规则属性 -->
+      <el-form
+        style="margin-top:30px"
+        :model="loginForm"
+        :rules="loginRules"
+        ref="formObj"
+        >
+        <el-form-item prop="mobile">
           <!-- 放置手机号 -->
-          <el-input placeholder="请输入手机号"></el-input>
+          <el-input
+            v-model="loginForm.mobile"
+            placeholder="请输入您的手机号"
+            ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <!-- 放置验证码 -->
-          <el-input style="width:280px" placeholder="请输入验证码"></el-input>
+          <el-input
+            style="width:280px"
+            v-model="loginForm.code"
+            placeholder="请输入您的验证码"
+            ></el-input>
           <el-button style="float:right" plain>发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议及条款</el-checkbox>
+        <el-form-item prop="checked">
+          <!-- 勾选同意框 -->
+          <el-checkbox v-model="loginForm.checked">我已阅读并同意用户协议及条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button
+            type="primary"
+            style="width:100%"
+            @click="onlogin"
+            >
+            登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -29,7 +49,53 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 要校验的整个表单数据
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: '' // 是否勾选
+      },
+      // 校验规则对象
+      loginRules: {
+        // key(要校验的字段名):value(数组-->多条或者一条或者没有规则)
+        mobile: [
+          { required: true, message: '请输入您的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }
+        ],
+        code: [
+          { required: true, message: '请输入您的验证码' },
+          { pattern: /^\d{6}$/, message: '验证码为6位数字' }
+        ],
+        checked: [
+          // rule 代表当前的规则
+          // value 代表当前的值 -->表单字段checked的值
+          // callBack 回调函数
+          { validator: function (rule, value, callBack) {
+            if (value) {
+              // 如果是true 就是选中了 通过校验
+              callBack() // 直接执行callBack,表示校验通过
+            } else {
+              // 没有选中 不通过校验
+              callBack(new Error('您必须同意才能使用'))
+            }
+          } }
+        ]
+      }
+    }
+  },
+  methods: {
+    onlogin () {
+      // ref: 可以通过给标签定义一个ref属性,通过this.$refs属性 获取dom对象
+      this.$refs.formObj.validate(function (isOK) {
+        if (isOK) {
+          // 如果为true 继续下一步,调用接口登录
+          console.log('校验成功')
+        }
+      })
+    }
+  }
 }
 </script>
 
